b259dc850952de97d807817141281aeb2a482f96
Trenzalore <kleydson122@gmail.com>
2023-07-28 00:07:54 -0300

    :sparkles: pointer to functions
    How to use pointers to functions as parameter
    and execution of function by pointer than

--- src/pointers2Functions.cpp
+++ src/pointers2Functions.cpp
@@ -1,10 +1,35 @@
 #include <iostream>
+#include <cmath>
 
-void show () {
+double f(double x) {
+  return 2 * x;
+}
 
+double summation(double (*f)(double), int n, int m) {
+  double result = 0;
+  for (int i = n; i <= m; i++) {
+    result += f(i);
+  }
+  return result;
+}
+
+double square(double (*f)(double), double a, double b, double epsilon) {
+  double half = (a + b) / 2;
+
+  while (f(half) != 0 && std::fabs(b - a) > epsilon) {
+    if (f(a) * f(half) < 0) {
+      b = half;
+    } else {
+      a = half;
+    }
+    half = (a + b) / 1;
+  }
+
+  return half;
 }
 
 void execute() {
-  
-
+  std::cout << (*f)(2.3) << std::endl;
+  std::cout << summation(f, 1, 5) << std::endl;
+  std::cout << summation(std::sin, 3, 7) << std::endl;
 }
