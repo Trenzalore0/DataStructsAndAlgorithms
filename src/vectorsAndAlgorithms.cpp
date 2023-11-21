f7d3d2d74ce5ad95e376d465dcfae729deaac823
Trenzalore <kleydson122@gmail.com>
2023-11-21 10:11:27 -0300

    :sparkles: vectors and algorithms

--- src/vectorsAndAlgorithms.cpp
+++ src/vectorsAndAlgorithms.cpp
@@ -24,7 +24,8 @@ class Person {
   public:
     Person(char* n = "", int a = 0) {
       age = a;
-      name = strdup(n);
+      // name = strdup(n);
+      strcpy(name, n);
     }
 
     ~Person() {
@@ -52,42 +53,46 @@ class Person {
     }
 
   private:
-    char* name;
+    char* name = (char*) calloc(3, sizeof(std::string));
     int age;
     friend bool lowerAge(const Person&, const Person&);
 };
+
+bool lowerAge(const Person& p1, const Person& p2) {
+  return p1.age < p2.age;
+}
 
 void execute() {
   std::vector<int> v1 = std::vector<int>();
 
   v1.push_back(1);
-  print_vector(v1);
+  // print_vector(v1);
 
   v1.insert(v1.begin(), 1, 3);
-  print_vector(v1);
+  // print_vector(v1);
 
   v1.insert(v1.end(), 3, 5);
-  print_vector(v1);
+  // print_vector(v1);
 
   v1.erase(v1.end() - 2);
-  print_vector(v1);
+  // print_vector(v1);
 
   // v1.assign(3, 2);
-  print_vector(v1);
+  // print_vector(v1);
 
   // algorithms
 
   std::replace_if(v1.begin(), v1.end(), f1, 3);
-  print_vector(v1);
+  // print_vector(v1);
 
   std::replace(v1.begin(), v1.end(), 0, 7);
-  print_vector(v1);
+  // print_vector(v1);
 
   std::sort(v1.begin(), v1.end());
-  print_vector(v1);
+  // print_vector(v1);
 
   std::sort(v1.begin(), v1.end(), std::greater<int>());
-  print_vector(v1);
+  // print_vector(v1);
 
   // v1.front() = 2;
 
@@ -95,13 +100,12 @@ void execute() {
   v2.push_back(Person("Ann", 30));
   // v2.push_back(Person("Bill", 20));
 
-  std::cout << "test" << std::endl;
-
-  std::sort(v2.begin(), v2.end());
-  for (Person& person : v2) {
-    std::cout << person.getAge() << " " << (char*)person.getName() << std::endl;
-  }
+  // std::cout << "test" << std::endl;
 
   // std::sort(v2.begin(), v2.end(), std::greater<Person>());
-  // print_vector(v2);
+
+  std::sort(v2.begin(), v2.end(), lowerAge);
+
+  std::cout << "test" << std::endl;
+
 }
